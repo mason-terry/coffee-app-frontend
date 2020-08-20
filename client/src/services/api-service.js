802b7a2f6783e4d8b2a7db65aab29c73f4b8acfb
Mason Terry <mason.terry@q2ebanking.com>
2020-08-20 16:21:18 -0500

    working on making pwa

--- client/src/services/api-service.js
+++ client/src/services/api-service.js
@@ -1,5 +1,6 @@
 import axios from 'axios'
 
 export const Api = axios.create({
-  baseURL: 'http://localhost:3000/api'
+  // baseURL: 'http://localhost:3000/api'
+  baseURL: 'http://91b5efe435f5.ngrok.io/api'
 })
